dc0c8ea9cef6efe21fb73c5cd459681a0fc9d140
KuntaePark <sgvelcro@naver.com>
2025-01-16 17:28:45 +0900

    20250116-3

--- 20250116/number.js
+++ 20250116/number.js
@@ -19,7 +19,27 @@ fn3 = () => {
   console.log(3 * 4);
 };
 fn3();
+
 //변수에 함수 저장 가능
 const v = f;
 v(3, 4);
 document.write("<button id='btn'>버튼</button>");
+
+const btn = document.getElementById("btn");
+
+btn.innerText = "새로운 버튼";
+
+let fs = 10;
+newName = () => {
+  const name = "더 새로운 버튼";
+  btn.innerText = name;
+  btn.style.color = "tomato";
+  fs += 10;
+  btn.style.fontSize = fs + "px";
+  btn.style.backgroundColor = "aliceblue";
+  btn.style.borderRadius = "10px";
+  btn.style.fontWeight = "bold";
+};
+
+// newName();
+btn.onclick = newName;
