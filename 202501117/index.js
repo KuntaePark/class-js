f8e5c611ddd9da11123840fade5ccdd8258e8ef1
KuntaePark <sgvelcro@naver.com>
2025-01-17 17:29:12 +0900

    20250117-2

---
+++ 202501117/index.js
@@ -0,0 +1,21 @@
+console.log("Hi");
+
+let a = 3;
+var b = 4;
+b = 6;
+
+const CONSTANT = 5;
+
+const f = () => {
+  return a * b;
+};
+console.log(f());
+
+let Apple = "Apple";
+Apple = "ms";
+
+//hoisting
+var d = 3;
+var d = "delta";
+
+console.log(Apple);
